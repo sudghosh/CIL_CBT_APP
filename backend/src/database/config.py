70bea28607e6b1aa6578f3031e26bc2e52002060
Sudipto Ghosh <binty.ghosh@gmail.com>
2025-08-10 12:53:09 +0530

    additions checkup not sure it is working or not

--- backend/src/database/config.py
+++ backend/src/database/config.py
@@ -6,6 +6,6 @@ load_dotenv()
 POSTGRES_USER = os.getenv('POSTGRES_USER')
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 POSTGRES_DB = os.getenv('POSTGRES_DB')
-POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'postgres')
+POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'localhost')
 
 DATABASE_URL = f'postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:5432/{POSTGRES_DB}'
